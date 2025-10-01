e2c6387ba971edfda359ba3c2f3fbb7ab99f3538
Edward <khielsgang@gmail.com>
2025-10-01 15:20:34 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -113,8 +113,8 @@ if (!profileImageUrl && screenshotBase64) {
     // Move significantly up and left to capture the profile picture
     const croppedBuffer = await sharp(screenshotBuffer)
       .extract({ 
-        left: 110, 
-        top: 150, 
+        left: 150, 
+        top: 160, 
         width: 120, 
         height: 120 
       })
@@ -261,6 +261,7 @@ IMPORTANT: Be funny through specificity and cleverness, not through listing buzz
     });
   }
 };
+
 
 
 
